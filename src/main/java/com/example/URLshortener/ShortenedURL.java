dd66e35215d946b4666b930ab009edc81a4f9f15
rwdale <ryan_dale@sfu.ca>
2020-08-29 20:28:57 -0700

    changed json format

--- src/main/java/com/example/URLshortener/ShortenedURL.java
+++ src/main/java/com/example/URLshortener/ShortenedURL.java
@@ -1,5 +1,9 @@
 package com.example.URLshortener;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerator;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -26,6 +30,8 @@ public class ShortenedURL {
     @CreationTimestamp
     private LocalDateTime created_at;
 
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+    @JsonIdentityReference(alwaysAsId = true)
     @OneToMany(targetEntity = UrlUse.class)
     @JoinColumn(name = "shortenedURL", referencedColumnName = "shortenedURL")
     private List<UrlUse> uses = new ArrayList<>();
